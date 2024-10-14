2e841a16c3a42ba411b5cb9868de121976a3cb0b
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2024-10-13 22:21:20 -0400

    Complete commit

---
+++ identity-service/src/main/java/com/devteria/identity/repository/httpclient/ProfileClient.java
@@ -0,0 +1,15 @@
+package com.devteria.identity.repository.httpclient;
+
+import com.devteria.identity.dto.request.ProfileCreationRequest;
+import com.devteria.identity.dto.response.UserProfileReponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "profile-service", url = "${app.profile-service.url}")
+public interface ProfileClient {
+
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    UserProfileReponse createProfile(@RequestBody ProfileCreationRequest request);
+}
